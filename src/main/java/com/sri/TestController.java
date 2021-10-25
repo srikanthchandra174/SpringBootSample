147110be73089c42bf4a9a13862ca1c27b86e88d
schandra19193 <64578565+schandra19193@users.noreply.github.com>
2021-10-25 13:12:15 +0530

    commit project

---
+++ src/main/java/com/sri/TestController.java
@@ -0,0 +1,15 @@
+package com.sri;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/")
+	public String hello() {
+		
+		return "Hello people from Earth";
+		
+	}
+
+}
